278531a2391a891a4bc1c143b0afcad8aa4074b5
s1919016 <m.macarie@student.utwente.nl>
2019-11-02 14:56:09 +0200

    Adding schemas for Widget and WidgetConfig

--- server/src/models/Widget.js
+++ server/src/models/Widget.js
@@ -1,1 +1,9 @@
 // widget related data
+  var mongoose = require('mongoose');
+  var Schema = mongoose.Schema;
+
+  var WidgetSchema = new Schema({
+	id: Number,
+    title:  String,
+    category: String,
+  });
---
+++ server/src/models/WidgetConfig.js
@@ -0,0 +1,8 @@
+// widget related data
+  var mongoose = require('mongoose');
+  var Schema = mongoose.Schema;
+
+  var WidgetSchemaSchema = new Schema({
+	WidgetID: Number,
+    property:  { name: String, value: String },,
+  });
